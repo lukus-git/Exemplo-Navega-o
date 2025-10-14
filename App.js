45a83f857885d205b5f2320c77499169f8eb880d
lucas Garbi <lucas.garbi13@hotmail.com>
2025-10-13 22:31:02 -0300

    segundo commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from './screens/DetailsScreen';
@@ -12,10 +13,30 @@ export default function App(){
   return(
     <NavigationContainer>
       <Tab.Navigator initialRouteName='Home'>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Details' component={DetailsScreen} />
-        <Tab.Screen name='About' component={AboutScreen} />
-        <Tab.Screen name='Product' component={ProductScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} options={{
+          title: 'Inicio',
+          tabBarIcon: ({color, size}) =>(
+            <Ionicons name='bag' color={color} size={size} />
+          )
+        }} />
+        <Tab.Screen name='Details' component={DetailsScreen} options={{
+          title: 'Detalhes',
+          tabBarIcon: ({color, size}) =>(
+            <Ionicons name='bag' color={color} size={size} />
+          )
+        }} />
+        <Tab.Screen name='About' component={AboutScreen} options={{
+          title: 'Sobre',
+          tabBarIcon: ({color, size}) =>(
+            <Ionicons name='bag' color={color} size={size} />
+          )
+        }} />
+        <Tab.Screen name='Product' component={ProductScreen} options={{
+          title: 'Produtos',
+          tabBarIcon: ({color, size}) =>(
+            <Ionicons name='bag' color={color} size={size} />
+          ) 
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   )
